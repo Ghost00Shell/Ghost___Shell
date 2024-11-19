a3f79f386817380c7cc7c7f3a8b046bce15b63d1
Ghost_Shell <91254737+Ghost00Shell@users.noreply.github.com>
2024-11-19 23:43:15 +0200

    Delete password.js

--- password.js
+++
@@ -1,8 +0,0 @@
-// api/password.js
-
-export default async function handler(req, res) {
-  const response = await fetch('https://api.npoint.io/8ba6d5e4994308131eac');
-  const data = await response.json();
-
-  res.status(200).json(data);  // Send the password data from the external API
-}
